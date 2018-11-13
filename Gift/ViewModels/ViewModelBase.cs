378482ecef2a7c1c31bbb2f4808b95a6db2fc70f
Christian Sieger <cs@labcognition.com>
2018-11-13 08:09:38 +0100

    add initial project

---
+++ Gift/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Gift.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
