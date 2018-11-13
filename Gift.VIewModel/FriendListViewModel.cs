6aa1df174bc2c7bba10d5339d17fe0b59b52f282
Christian Sieger <cs@labcognition.com>
2018-11-13 11:40:12 +0100

    add friendlistviewmodel

---
+++ Gift.VIewModel/FriendListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ReactiveUI;
+using System;
+using SharpTox;
+using SharpTox.Core;
+using System.Collections.Generic;
+using ReactiveUI.Fody.Helpers;
+using System.Reactive.Linq;
+
+namespace Gift.ViewModel
+{
+    public class FriendListViewModel : ReactiveObject
+    {
+        public FriendListViewModel(Tox tox, ICollection<int> friends)
+        {
+        }
+    }
+
+    public class FriendListEntryViewModel : ReactiveObject
+    {
+        public FriendListEntryViewModel(Tox tox, int friendNumber)
+        {
+            var publicKey = tox.GetFriendPublicKey(friendNumber);
+            //EventHandler<ToxEventArgs.StatusEventArgs
+            //Observable.FromEventPattern<ToxEventArgs.FriendConnectionStatusEventArgs>(h => tox.OnFriendConnectionStatusChanged += h, h => tox.OnFriendConnectionStatusChanged -= h)
+            //tox.GetFriendStatus
+        }
+
+        [ObservableAsProperty]
+        public bool IsAway { get; }
+    }
+}
